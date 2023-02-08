aa1e8b3013f8b39cc8d59c3d9a6e9102f1383475
komandosscz17 <95014999+komandosscz17@users.noreply.github.com>
2023-02-08 10:51:16 +0100

    Delete projekt directory

--- projekt/main.py
+++
@@ -1,5 +0,0 @@
-from projekt_odevzdat import GUI
-
-if __name__ == "__main__":
-    app = GUI()
-    app.mainloop()
--- projekt/uklad.py
+++
@@ -1,8 +0,0 @@
-class ulozit:
-    def __init__(self, file_name):
-        self.file_name = file_name
-    
-    def save(self, passwords):
-        with open(self.file_name, "a") as file:
-            for password in passwords:
-                file.write(password + "\n")
